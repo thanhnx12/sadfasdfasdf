17ec18b77f3b1e2896acad5a06d1d4743ca67f3c
thanhnx12 <thanhnx1209@gmail.com>
2024-05-24 05:42:38 +0700

    change key gemini

--- utils.py
+++ utils.py
@@ -147,13 +147,9 @@ def gpt(input, t=0, key=None):
     # return completion.choices[0].message.content
     time.sleep(1)
     # genai.configure(api_key = 'AIzaSyD0XqYHI6UKQDaXLkHfdLh8UnFkGTw5_LI' )
-    generation_config = {
-    "temperature": 0.0,
-    "response_mime_type": "text/plain",
-    }
     genai.configure(api_key='AIzaSyDBECQnpdlHjyw0m90b8nMRBsA_oaE0WXU')
     model = genai.GenerativeModel('gemini-1.5-pro-latest')
-    response = model.generate_content(input,generation_config=generation_config)
+    response = model.generate_content(input)
     return response.text
 @retry(tries=10 , delay = 1)
 def gemini(input, t=0,key=None):
